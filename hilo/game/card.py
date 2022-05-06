44f869775624b94a362e80bca73a204d269e594e
Emma Lund <emma.lund22@gmail.com>
2022-05-05 20:54:53 -0600

    methods

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,17 +1,18 @@
 import random
 class Card:
-    number = 0
+    def __init__(self):
+        self.number = random.randint(1,13)
     """
     Individual cards are represented as a number from 1 to 13.
 
     Attributes:
         number (int): The value of the card
     """
-    def set_number(self):
-        self.number = random.randint(1,13)
+    def get_number(self):
+        return self.number
     
+    def display_number(self):
+        print(f"The card is: {self.number}")
     """
     This method will display the number obtained by the set_number method.
     """
-    def display_number():
-        pass
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,3 +1,4 @@
+from hilo.game.card import Card
 class Player:
     """
         The player starts the game with 300 points
@@ -9,13 +10,49 @@ class Player:
         Attributes:
             points (int): keep track of the player's success in the game
     """
-    points = 300
+    def __init__(self):
+        self.points = 300
+        self.card = Card()
+        '''
+        Constructs a new instance of Player with a points and a card attribute.
 
-    def guess():
-        pass
+        Args:
+            self (Player): an instance of a Player
+        '''
+
+    def guess(self):
+        higher_or_lower = input("Higher or lower? [h/l]")
+        next_card = Card()
+        print(f"Next card was: {next_card.get_number()}")
+        if higher_or_lower == 'h':
+            if next_card.get_number() > self.card.get_number():
+                return True
+            else:
+                return False
+        elif higher_or_lower == 'l':
+            if next_card.get_number() < self.card.get_number():
+                return True
+            else:
+                return False
+
 
     def change_points(self):
-        pass
+        '''
+        Change points according to accuracy of player's guess;
+        The player earns 100 points if they guess correctly and
+        the player loses 75 points if they guessed incorrectly
+        '''
+        if self.guess(self):
+            points += 100
+        else:
+            points -= 75
 
-    def play_again():
-        pass
+    def display_points(self):
+        print(f"Your score is: {self.points}")
+
+    def play_again(self):
+        if self.points > 0:
+            yes_or_no = input("Play again? [y/n]")
+            #prompt another round
+        else:
+            return("Thanks for playing!")
