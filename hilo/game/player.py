2d601287aaf4f7022d5a67a1f2c790aff6b71a64
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-05-05 22:57:00 -0400

    player method comments

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -11,11 +11,35 @@ class Player:
     """
     points = 300
 
+    def __init__(self):
+        """Constructs a new Player.
+        
+        Args:
+            self (Player): an instance of Player.
+        """
+        self.points = 300
+        pass
+
     def guess():
+        """Ask the player for the number to guess if is greater or lower.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         pass
 
     def change_points(self):
+        """Updates the player's score.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         pass
 
     def play_again():
+        """Asks the player to play again.
+
+        Args:
+            self (Player): An instance of Player.
+        """
         pass
