a623d20078d8bfbe4de1cded7f5aa5e2120e7347
Kennette Guevara <92239496+SatoshiKenn@users.noreply.github.com>
2022-05-05 22:03:36 -0400

    set number comments

--- card.py
+++ card.py
@@ -1,7 +1,12 @@
 import random
 class Card:
     number = 0
+    """
+    Individual cards are represented as a number from 1 to 13.
 
+    Attributes:
+        number (int): The number of the different cards.
+    """
     def set_number(self):
         self.number = random.randint(1,13)
     
