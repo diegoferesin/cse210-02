89969a114fe8593efaf4e798cd1fc094e0fc83a3
CastellaniJuan <castellanijuanignacio@gmail.com>
2022-05-14 19:59:00 -0300

    update

--- hilo/game/game.py
+++ hilo/game/game.py
@@ -1,5 +1,5 @@
-from player import Player
-from card import Card
+import player
+import card
 
 class Game:
 
@@ -12,7 +12,7 @@ class Game:
 
     def start_game(self):
 
-        player1 = Player()
+        player1 = player.Player()
 
         """
         An intance of player is created.
@@ -31,15 +31,15 @@ class Game:
 
 
 
-        def play(player):
-            player.display_card()
+        def play(player1):
+            player1.display_card()
             #The current card is deplayed
 
             guess = input("Higher or lower? [h/l] ")
             if guess.lower() != "h" and guess.lower() != "l":
                 print("Incorrect entry, please use 'h' or 'l'.")
             else:
-                new_card = Card()
+                new_card = card.Card()
                 print(f"Next card was: {new_card.get_number()}")
                 """
                 The new card is stores and displayed.
@@ -61,23 +61,23 @@ class Game:
 
                 """        
 
-                player.card = new_card
+                player1.card = new_card
             
 
-        def change_points(player, guess):
+        def change_points(player1, guess):
             """Updates the player's score according to the accuracy of their guess
 
             Args:
                 self (Player): An instance of Player.
             """
             if guess==True:
-                player.points+=100
+                player1.points+=100
             elif guess==False:
-                player.points-=75
+                player1.points-=75
 
-        def end_game(self, player):
-            if player.points <= 0:
-                player.points = 0
+        def end_game(self, player1):
+            if player1.points <= 0:
+                player1.points = 0
                 self.isPlaying=False
                 print("You ran out of point to play.")
 
@@ -85,9 +85,9 @@ class Game:
                 In order to be more visual appealing the score will not go under 0.
                 """
 
-                player.display_points()   
+                player1.display_points()   
                 #points displayed.
-            if player.points != 0:
+            if player1.points != 0:
                 while True:  
                     play = input("Play again? [y/n] ")
                     if play.lower() != "y" and play.lower() != "n":
